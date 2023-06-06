ca5d2bb63a4f7e772380fba4d834d85e89031c53
Ketch <teguh.budmn@gmail.com>
2023-06-06 22:22:30 +0700

    menghapus bagian courses yang tidak digunakan

--- application/views/web/home.php
+++ application/views/web/home.php
@@ -62,9 +62,10 @@
                     <ul class="menu-area-main">
                       <li class="active"> <a href="index.html">Home</a> </li>
                       <li> <a href="#important">MAP</a> </li>
-                      <li> <a href="#courses">My Courses </a> </li>
+
                       <li> <a href="#about">About</a> </li>
                       <li> <a href="#learn">My Profile</a> </li>
+
                       <li> <a href="#contact">Contact</a> </li>
                      </ul>
                    </nav>
@@ -220,70 +221,7 @@
 
 
 <!-- end our -->
-<!-- Courses -->
-<div id="courses" class="Courses">
-  <div class="container-fluid padding_left3">
-    <div class="row">
-      <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-        <div class="box_bg">
-          <div class="box_bg_img">
-            <div class="row">
-              <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                <div class="box_my">
-                  <figure><img src="<?=base_url()?>assets/web/images/my1.jpg"></figure>
-                  <div class="overlay">
-                    <h3>Data Structures</h3>
-                    <p>It is a long established fact that a reader will be distracted by the readable content o</p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                <div class="box_my">
-                  <figure><img src="<?=base_url()?>assets/web/images/my2.jpg"></figure>
-                  <div class="overlay">
-                    <h3>Cinematography</h3>
-                    <p>It is a long established fact that a reader will be distracted by the readable content o</p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                <div class="box_my">
-                  <figure><img src="<?=base_url()?>assets/web/images/my3.jpg"></figure>
-                  <div class="overlay">
-                    <h3>Skills</h3>
-                    <p>It is a long established fact that a reader will be distracted by the readable content o</p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                <div class="box_my">
-                  <figure><img src="<?=base_url()?>assets/web/images/my4.jpg"></figure>
-                  <div class="overlay">
-                    <h3>Teaching Science</h3>
-                    <p>It is a long established fact that a reader will be distracted by the readable content o</p>
-                  </div>
-                </div>
-              </div>
 
-
-
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 border_right">
-        <div class="box_text">
-          <div class="titlepage">
-            <h2>My <strong class="yellow"> Courses</strong></h2>
-          </div>
-          <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-          <a href="Javascript:void(0)">Read more</a>
-        </div>
-      </div> 
-    </div>
-  </div>
-</div>
-<!-- end Courses -->
 
 <!-- learn -->
 
